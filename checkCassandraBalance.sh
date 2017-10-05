f04a86a7a71ed13f7661bbb40094195ee7e84c72
root <root@origin3.neterra.tv>
2017-10-05 16:33:58 +0300

    first output

--- checkCassandraBalance.sh
+++ checkCassandraBalance.sh
@@ -8,3 +8,21 @@ if [ $checkNodetool -ne 0 ]; then
 	echo "CRITICAL nodetool not found"
 	exit 2
 fi
+
+nodetoolCommand=$($nodetoolBin status | grep UN)
+
+IFS='UN' read -r -a nodes <<< $nodetoolCommand
+
+for line in "${nodes[@]}"
+do
+	IFS=' ' read -r -a array <<< $line
+	nodeData=$(echo ${array[1]} | tr '\n' ' ')
+	nagiosLine=$nagiosLine$nodeData
+#	echo ${array[2]} | tr '\n' ' '
+#	for element in "${array[@]}"
+#	do
+#		echo $element
+#	done
+done
+
+echo $nagiosLine
