de2f0e0e91b351e8fd05c3ab7d75037ea9e5368f
root <root@origin3.neterra.tv>
2017-10-13 11:45:23 +0300

    add pct

--- checkCassandraBalance.sh
+++ checkCassandraBalance.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 nodetoolBin="nodetool"
+bcBin="bc"
 
 hash $nodetoolBin 2>/dev/null
 checkNodetool=$?
 if [ $checkNodetool -ne 0 ]; then
 	echo "CRITICAL nodetool not found"
 	exit 2
+fi
+
+hash $bcBin 2>/dev/null
+checkBC=$?
+if [ $checkBC -ne 0 ]; then
+        echo "CRITICAL bc not found. Try to install bc"
+        exit 2
 fi
 
 nodetoolCommand=$($nodetoolBin status | grep UN)
@@ -19,18 +27,24 @@ do
 	IFS=' ' read -r -a array <<< $line
 	nodeIP=$(echo ${array[0]} | sed -e 's/^[ \t]*//')
 	nodeData=$(echo ${array[1]} | sed -e 's/^[ \t]*//')
-
+	nodeDataDimension=$(echo ${array[2]} | sed -e 's/^[ \t]*//')
+	nodePct=$(echo ${array[4]} | sed -e 's/^[ \t]*//' | sed 's/.$//')
 	if [ ! -z $nodeIP ] || [ ! -z $nodeData ];then
+		if [[ $nodeDataDimension =~ ^MiB$ ]]; then
+			nodeData=$( echo "scale=2; $nodeData / 1024" | bc | sed 's/^\./0./' )
+		fi
 		if [ $counter -eq 0 ]; then
                         nagiosLine="$nodeIP=$nodeData"
-			nagiosLinePNP="$nodeIP=$nodeData;"
+			nagiosLineDataPNP="$nodeIP"_data"=$nodeData;"
+			nagiosLinePctPNP="$nodeIP"_pct"=$nodePct;"
                 else
                         nagiosLine=$nagiosLine" $nodeIP=$nodeData"
-			nagiosLinePNP=$nagiosLinePNP" $nodeIP=$nodeData;"
+			nagiosLineDataPNP=$nagiosLineDataPNP" $nodeIP"_data"=$nodeData;"
+			nagiosLinePctPNP=$nagiosLinePctPNP" $nodeIP"_pct"=$nodePct;"
                 fi
 	((counter++))
 	fi
 done
 
-echo "$nagiosLine | $nagiosLinePNP"
+echo "$nagiosLine | $nagiosLineDataPNP $nagiosLinePctPNP"
 exit 0
