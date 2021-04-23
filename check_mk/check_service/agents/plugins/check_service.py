2d964b7ce717055945688e373aae33bf5c1385c1
antonzhelyazkov <antonisimo@gmail.com>
2021-04-23 23:07:22 +0300

    check_mk

--- check_mk/check_service/agents/plugins/check_service.py
+++ check_mk/check_service/agents/plugins/check_service.py
@@ -1,16 +1,26 @@
 import json
 import os
 import sys
+import subprocess
 
 config_file_name = os.path.basename(sys.argv[0]).split('.')[0]
 CONFIG_FILE = f"/etc/check_mk/{config_file_name}"
 
 
+def get_services(in_json: dict):
+    pass
+
+
 def main():
+    config = None
     with open(CONFIG_FILE, 'r') as config_handler:
         config_raw = config_handler.read()
 
-    config = json.loads(config_raw)
+    try:
+        config = json.loads(config_raw)
+    except json.decoder.JSONDecodeError as conf_err:
+        print(f"ERROR config {conf_err}")
+
     print(config)
 
 
