f7ac59c530ee5f10175c60c64e913ac7e4fcb60e
antonzhelyazkov <antonisimo@gmail.com>
2021-04-24 10:24:09 +0300

    check_mk

--- check_mk/check_service/agents/plugins/check_service.py
+++ check_mk/check_service/agents/plugins/check_service.py
@@ -25,7 +25,8 @@ def main():
         print(f"ERROR config {conf_err}")
         return False
 
-    service = filter(lambda seq: is_service(config[seq]), config.keys())
+    # service = filter(lambda seq: is_service(config[seq]), config.keys())
+    service = filter(lambda seq: config[seq]['type'] == 'service', config.keys())
     print(list(service))
 
 
